fe0a38baada7b736feee292173f81a6c51c23477
Anten Skrabec <askrabec@redhat.com>
2020-10-01 01:14:16 -0600

    fix tests and delete unused code

--- util/config.go
+++ util/config.go
@@ -1,10 +1,6 @@
 package util
 
 import (
-	"bufio"
-	"os"
-	"strings"
-
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -57,47 +53,9 @@ func ReadConfig() {
 	log.Info().Msgf("[LOG] Log Level set to: %s", globalLevel.String())
 
 	if err != nil {
-		log.Info().Msgf("[CONF] Supplied debugging log level (%d) is invalid. Defaulting to \"info\".", Cfg.Wadl.LogLevel)
+		log.Info().Msgf("[CONF] Supplied debugging log level (%s) is invalid. Defaulting to \"info\".", Cfg.Wadl.LogLevel)
 	}
 
 	// Set global log level
 	zerolog.SetGlobalLevel(globalLevel)
-}
-
-func parseLogLevel(lglvl int) zerolog.Level {
-	var final zerolog.Level = zerolog.InfoLevel
-	switch lglvl {
-	case 0:
-		final = zerolog.WarnLevel
-		break
-	case 1:
-		final = zerolog.InfoLevel
-		break
-	case 2:
-		final = zerolog.DebugLevel
-		break
-	default:
-		final = zerolog.InfoLevel
-		break
-	}
-	return final
-}
-
-func getToken(filepath string) (string, error) {
-	file, err := os.Open(filepath)
-
-	if err != nil {
-		return "", err
-	}
-
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		if strings.Split(scanner.Text(), "=")[0] == "token" {
-			return strings.Split(scanner.Text(), "=")[1], nil
-		}
-	}
-	return "", err
 }
