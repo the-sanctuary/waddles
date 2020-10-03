24a3057d30d04aeda8f2f6cafab4eabd3fdec172
Anten Skrabec <askrabec@redhat.com>
2020-10-03 03:58:47 -0600

    remove unused code

--- handler/handler.go
+++ handler/handler.go
@@ -5,21 +5,9 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func MsgHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all message if they come from Waddles
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-
-	if m.Content == "!ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-}
-
 // TraceAllMessages sends a Trace message to the log with showing every message processed
 func TraceAllMessages(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
