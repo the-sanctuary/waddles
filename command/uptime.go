5e664d37cbeccf12efd3ea7cb9b6c83f9cf53cdf
Anten Skrabec <cebarks@gmail.com>
2020-10-05 00:56:23 -0600

    add rounding to uptime command

--- command/uptime.go
+++ command/uptime.go
@@ -12,6 +12,6 @@ var uptime *Command = &Command{
 	Usage:       "uptime",
 	Handler: func(c *Context) {
 		c.ReplyStringf("Current Bot Time: `%s`", time.Now().Format(time.RFC1123Z))
-		c.ReplyStringf("Uptime: `%s`", util.Uptime())
+		c.ReplyStringf("Uptime: `%s`", util.Uptime().Round(time.Second))
 	},
 }
